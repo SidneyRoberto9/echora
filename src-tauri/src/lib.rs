b1b7b732a04761127432b5c8586a79eea604f00b
Sidney Roberto <sidneyrpsilva@gmail.com>
2026-09-01 18:59:44 -0300

    feat(packaging): register shell plugin, externalBin, dev binary layout

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -39,6 +39,7 @@ pub fn run() {
         .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             let app_dir = app
                 .path()
